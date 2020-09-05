305371309a7b74ce6506a75f1ee9545a126666ac
swssr <simocmo4@gmail.com>
2020-09-05 15:55:19 +0200

    Add google maps

--- src/Pages/Nearby.js
+++ src/Pages/Nearby.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchForm from "../Components/SearchForm";
 import RestaurantCard from "../Components/RestaurantCard";
 import NavTop from "../Components/NavTop";
 
+import GoogleMaps from "google-map-react";
+
 export default function Nearby() {
+  const [coord, setCoord] = useState();
+
+  const [pos, error] = useGeoLocation();
   const restaurants = Array(Math.ceil(2 + Math.random() * 10)).fill(null);
+  console.log(process.env.GOOGLE_MAPS_KEY);
+  if (error) alert("Failed to get your geolocation.");
   return (
     <div className="page page--z-mid">
       <NavTop>
         <SearchForm />
       </NavTop>
-      <div className="map"></div>
+      <div className="map">
+        <GoogleMaps bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS_KEY }} />
+      </div>
       <div className="places">
         <h2>Nearby Places</h2>
         <div className="overflow-wrap">
@@ -26,4 +35,23 @@ export default function Nearby() {
       </div>
     </div>
   );
+}
+
+function useGeoLocation() {
+  const [pos, setPos] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    return navigator.geolocation.getCurrentPosition(
+      (_pos) => setPos(_pos),
+      (error) => setError(error),
+      {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 10000,
+      }
+    );
+  }, []);
+
+  return [pos, error];
 }
