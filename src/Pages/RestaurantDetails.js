4c19efc1b072aa129347bdd9279ec818c593ecd2
swssr <simocmo4@gmail.com>
2020-09-05 22:38:16 +0200

    Add base restaurant details

---
+++ src/Pages/RestaurantDetails.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
+
+export default function RestaurantDetails() {
+  const location = useLocation();
+  return (
+    <div className="page page--details">
+      <h1>Restaurant</h1>
+      <p>Open Now</p>
+      <p>
+        Call <strong>079 644 1784</strong> <br />
+        Email <strong>restaurant@mail.com</strong>
+      </p>
+      <Link
+        to={{ pathname: "/booking", state: { ...location.state } }}
+        className="btn btn--primary"
+      >
+        Book a table
+      </Link>
+      <p>
+        <h4>Menu</h4>
+        <ul>
+          <li>
+            <h4>Fancy meal 1</h4>
+            <p>fancy description</p>
+          </li>
+          <li>
+            <h4>Fancy meal 2</h4>
+            <p>fancy description</p>
+          </li>
+          <li>
+            <h4>Fancy meal 3</h4>
+            <p>fancy description</p>
+          </li>
+          <li>
+            <h4>Fancy meal 4</h4>
+            <p>fancy description</p>
+          </li>
+          <li>
+            <h4>Fancy meal 5</h4>
+            <p>fancy description</p>
+          </li>
+        </ul>
+      </p>
+    </div>
+  );
+}
