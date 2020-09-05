7cf7ac9590d9f3497f159e1fddd22a3d4bc0f4b8
swssr <simocmo4@gmail.com>
2020-09-05 06:52:04 +0200

    Fix num reeview rounding error

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -10,16 +10,18 @@ export default function RestaurantCard({
 }) {
   return (
     <figure className="card">
-      <RandomImage />
-      <button>{isSaved && <span className="icon icon--saved"></span>}</button>
+      <RandomImage className="img img--bg" />
+      <button className="btn btn--badge">
+        {isSaved && <span className="icon icon--saved"></span>}
+      </button>
       <figcaption>
         <div>
           <h3>{name}</h3>
           <span className="rating">
             <span className="num-reviews">
-              {Math.ceil(2 + Math.random() * 100).toFixed(1)}
+              {Math.ceil(2 + Math.random() * 100)}
             </span>
-            <span className="score">{Math.round(Math.random() * 5)}</span>
+            <strong className="score">{(Math.random() * 5).toFixed(1)}</strong>
             <i class="fas fa-star"></i>
           </span>
         </div>
