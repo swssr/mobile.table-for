ad0adec81ceed45c7045543b178d73a842205e2b
swssr <simocmo4@gmail.com>
2020-09-05 21:23:05 +0200

    Clean debug styles

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,10 +1,30 @@
 import React, { useState } from "react";
+import Fuse from "fuse.js";
 
-export default function SearchForm() {
-  const [query, setQuery] = useState();
+export default function SearchForm(props) {
+  const [query, setQuery] = useState("");
 
   const handleSearch = (event) => {
     event.preventDefault();
+    const options = {
+      // isCaseSensitive: false,
+      // includeScore: false,
+      // shouldSort: true,
+      // includeMatches: false,
+      // findAllMatches: false,
+      // minMatchCharLength: 1,
+      // location: 0,
+      // threshold: 0.6,
+      // distance: 100,
+      // useExtendedSearch: false,
+      // ignoreLocation: false,
+      // ignoreFieldNorm: false,
+      keys: ["name", "cuisine", ""],
+    };
+
+    const fuse = new Fuse(props.restaurants, options);
+
+    return fuse.search(query);
   };
   return (
     <form className="form" onSubmit={handleSearch}>
