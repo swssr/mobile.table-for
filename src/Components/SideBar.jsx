d45e378cc01e5841df740ded54247d77950d9c98
swssr <simocmo4@gmail.com>
2020-09-13 11:47:04 +0200

    Add links to nav

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+
+import { Link } from "react-router-dom";
 import { useThemeState } from "../context";
 
 export default function SideBar() {
@@ -6,6 +8,24 @@ export default function SideBar() {
 	return (
 		<aside
 			className={`sidebar ${themeState.sidebarOpen ? "sidebar--open" : ""}`}
-		></aside>
+		>
+			<ul className="sidebar__items">
+				<li className="sidebar__item">
+					<Link to="/login" className="link link--major">
+						Link 1
+					</Link>
+				</li>
+				<li className="sidebar__item">
+					<Link to="/register" className="link link--major">
+						Link 2
+					</Link>
+				</li>
+				<li className="sidebar__item">
+					<Link to="/" className="link link--major">
+						Link 3
+					</Link>
+				</li>
+			</ul>
+		</aside>
 	);
 }
