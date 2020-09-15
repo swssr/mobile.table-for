06520103660ae44201cf7be073fb00cbe8cfd8f5
swssr <simocmo4@gmail.com>
2020-09-15 10:51:48 +0200

    Add remote API url

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,7 @@ const IS_LIVE = !process.env.NODE_ENV === "development";
 
 module.exports = {
 	IS_LIVE,
-	API_URL: IS_LIVE ? "" : "http://localhost:8080",
+	API_URL: IS_LIVE
+		? "https://tablefor.ew.r.appspot.com"
+		: "http://localhost:8080",
 };
