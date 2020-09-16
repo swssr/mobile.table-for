4e9ed26c2563c13b73a2cd4469dd8ba6188ef4cb
swssr <simocmo4@gmail.com>
2020-09-16 17:30:42 +0200

    Fix API url

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@ const IS_LIVE = !process.env.NODE_ENV === "development";
 module.exports = {
 	IS_LIVE,
 	API_URL: IS_LIVE
-		? "http://localhost:8080"
-		: "https://tablefor.ew.r.appspot.com",
+		? "https://tablefor.ew.r.appspot.com"
+		: "http://localhost:8080",
 };
