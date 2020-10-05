48cf72b5e031310e071c8508872459af72381150
swssr <simocmo4@gmail.com>
2020-10-05 09:57:53 +0200

    More clean up

--- src/Pages/Saved.jsx
+++ src/Pages/Saved.jsx
@@ -4,29 +4,29 @@ import RestaurantCard from "../Components/RestaurantCard";
 import { fetcher } from "../helpers";
 
 export default function Saved() {
-	const { data: rest } = useSWR("/restaurant/list", fetcher);
-	const { data: pro } = useSWR("/auth/profile", fetcher);
+  const { data: rest } = useSWR("/restaurant/list", fetcher);
+  const { data: pro } = useSWR("/auth/profile", fetcher);
 
-	const [saved, set] = useState([]);
+  const [saved, set] = useState([]);
 
-	useEffect(() => {
-		if (!(rest && pro)) return;
-		// return console.log(book);
-		set(
-			pro.saveLocations.map((id) => {
-				const _r = rest.restaurants.find((r) => r._id === id);
-				return _r;
-			})
-		);
-	}, [rest, pro]);
-	return (
-		<div className="container container--saved">
-			<h1 className="page-head">Saved Restaurants</h1>
-			<section className="list">
-				{saved.map((r) => (
-					<RestaurantCard {...r} />
-				))}
-			</section>
-		</div>
-	);
+  useEffect(() => {
+    if (!(rest && pro)) return;
+    // return console.log(book);
+    set(
+      pro.saveLocations.map((id) => {
+        const _r = rest.restaurants.find((r) => r._id === id);
+        return _r;
+      })
+    );
+  }, [rest, pro]);
+  return (
+    <div className="container container--saved">
+      <h1 className="page-head">Saved Restaurants</h1>
+      <section className="list">
+        {saved.map((r) => (
+          <RestaurantCard isSaved={true} {...r} />
+        ))}
+      </section>
+    </div>
+  );
 }
