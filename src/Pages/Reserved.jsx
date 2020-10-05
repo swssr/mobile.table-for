feba3e4bc3509758f2849541a95926adc1b8d427
swssr <simocmo4@gmail.com>
2020-10-05 09:51:10 +0200

    Clean up

--- src/Pages/Reserved.jsx
+++ src/Pages/Reserved.jsx
@@ -5,64 +5,63 @@ import RestaurantCard from "../Components/RestaurantCard";
 import { fetcher } from "../helpers";
 
 export default function Reserved() {
-	const { data: book, error, loading } = useSWR("/booking", fetcher);
-	const { data: rest } = useSWR("/restaurant/list", fetcher);
-	const { data: pro } = useSWR("/auth/profile", fetcher);
+  const { data: book, error, loading } = useSWR("/booking", fetcher);
+  const { data: rest } = useSWR("/restaurant/list", fetcher);
+  const { data: pro } = useSWR("/auth/profile", fetcher);
 
-	const [reserved, set] = useState([]);
+  const [reserved, set] = useState([]);
 
-	useEffect(() => {
-		if (!(rest && pro && book)) return;
-		// return console.log(book);
-		set(
-			book.map((b) => {
-				const _r = rest.restaurants.find((r) => r._id == b.restaurantId);
-				return { ...b, ..._r };
-			})
-		);
-	}, [rest, book, pro]);
-	return (
-		<div className="container container--reserved">
-			<header>
-				<h1 className="page-head">Reserved</h1>
-				<p>({reserved.length}) historicaly</p>
-			</header>
+  useEffect(() => {
+    if (!(rest && pro && book)) return;
+    // return console.log(book);
+    set(
+      book.map((b) => {
+        const _r = rest.restaurants.find((r) => r._id == b.restaurantId);
+        return { ...b, ..._r };
+      })
+    );
+  }, [rest, book, pro]);
+  return (
+    <div className="page page--reserved">
+      <header>
+        <h1 className="page-head">Reserved</h1>
+        <p>({reserved.length}) historicaly</p>
+      </header>
 
-			<ul className="list">
-				{reserved.map((r, index) => (
-					<BookingCard {...r} />
-				))}
-			</ul>
-		</div>
-	);
+      <ul className="list">
+        {reserved
+          .slice()
+          .reverse()
+          .filter((r) => r.name)
+          .map((r, index) => (
+            <BookingCard key={`${r._id + index}`} {...r} />
+          ))}
+					<div className="spacer"></div>
+					<div className="spacer"></div>
+      </ul>
+    </div>
+  );
 }
 
 function BookingCard({ name, address, opens, closes, isSaved, ...rest }) {
-	return (
-		<figure className="card">
-			<RandomImage className="img img--bg" />
-			<button className="btn btn--badge">
-				{isSaved && <span className="icon icon--saved"></span>}
-			</button>
-			<figcaption>
-				<div>
-					<h3>{name}</h3>
-					<span className="rating">
-						<span className="num-reviews">
-							{Math.ceil(2 + Math.random() * 100)}
-						</span>
-						<strong className="score">{(Math.random() * 5).toFixed(1)}</strong>
-						<i class="fas fa-star"></i>
-					</span>
-				</div>
-				<div>
-					<p>{new Date(rest.date).toLocaleString()}</p>
-					<p className="location">{address}</p>
-					<p className="location">
-						Booked <strong>{rest.seats}</strong> Seats
-					</p>
-				</div>
-			</figcaption>
-		</figure>
-	);
+  return (
+    <figure className="card">
+      <RandomImage className="img img--bg" />
+      <button className="btn btn--badge">
+        {isSaved && <span className="icon icon--saved"></span>}
+      </button>
+      <figcaption>
+        <div>
+          <h3>{name}</h3>
+        </div>
+        <div>
+          <p>{new Date(rest.date).toLocaleString()}</p>
+          <p className="location">{address}</p>
+          <p className="location">
+            Booked <strong>{rest.seats}</strong> Seats
+          </p>
+        </div>
+      </figcaption>
+    </figure>
+  );
 }
--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 const IS_LIVE = process.env.NODE_ENV !== "development";
 
 module.exports = {
-	IS_LIVE,
-	API_URL: IS_LIVE
-		? "https://tablefor.ew.r.appspot.com"
-		: "http://localhost:8080",
-	// API_URL: "https://tablefor.ew.r.appspot.com",
-	// : "http://localhost:8080",
+  IS_LIVE,
+  // API_URL: IS_LIVE
+  // 	? "https://tablefor.ew.r.appspot.com"
+  // 	: "http://localhost:8080",
+  API_URL: "https://tablefor.ew.r.appspot.com",
+  // : "http://localhost:8080",
 };
